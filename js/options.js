180079afb2dbaa3be98a0aeef6bd4618b5b5f3f2
nomandhoni-cs <nomandhoni@yahoo.com>
2023-06-13 18:16:42 +0600

    Added protected route

--- js/options.js
+++ js/options.js
@@ -11,9 +11,10 @@ const apiKeyInputOptionsPage = document.getElementById(
 const usernameInputOptionsPage = document.getElementById(
   "api-username-input-options-page"
 );
+
 // Fetch user info function from Notification
 function fetchUserInfo(apiKey) {
-  fetch("https://cache.showwcase.com/notifications", {
+  fetch("https://cache.showwcase.com/auth", {
     headers: {
       "Content-Type": "application/json",
       "x-api-key": apiKey,
@@ -26,16 +27,17 @@ function fetchUserInfo(apiKey) {
       return response.json();
     })
     .then((data) => {
-      if (data[0].data.thread) {
-        const user = data[0].data.thread.user;
+      if (data) {
+        const user = data;
         const userJson = JSON.stringify(user);
-        chrome.storage.local.set({ userInfo: userJson });
+        localStorage.setItem("userInfo", userJson);
       }
     })
     .catch((error) => {
       console.error("There was a problem with the fetch operation:", error);
     });
 }
+
 // Form submit event listener
 formInOptionsPage.addEventListener("submit", (event) => {
   event.preventDefault(); // Prevent form submission
@@ -46,46 +48,42 @@ formInOptionsPage.addEventListener("submit", (event) => {
   console.log("API Key:", apiKey);
   console.log("Username:", username);
 
-  // Save API key to Chrome storage
-  chrome.storage.local.set({ "showwcase-api-key": apiKey }, function () {
-    // Change the text to submitted but not change the svg icon in the btn
-    formSubmitBtnOptionsPage.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+  // Save API key to local storage
+  localStorage.setItem("showwcase-api-key", apiKey);
+  // Change the text to submitted but not change the svg icon in the btn
+  formSubmitBtnOptionsPage.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
     </svg> Submitted!`;
-    // Change the text back to submit after 3 seconds
-    setTimeout(() => {
-      formSubmitBtnOptionsPage.innerHTML = `<svg class="paper-plane" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+  // Change the text back to submit after 3 seconds
+  setTimeout(() => {
+    formSubmitBtnOptionsPage.innerHTML = `<svg class="paper-plane" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
       aria-hidden="true">
       <path
         d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z">
       </path>
     </svg> Submit`;
-    }, 2000);
-    console.log("API key saved to Chrome storage");
-  });
+  }, 2000);
+  console.log("API key saved to local storage");
 
-  // Save username to Chrome storage
-  chrome.storage.local.set({ "showwcase-username": username }, function () {
-    console.log("Username saved to Chrome storage");
-  });
-  chrome.storage.local.get(["userInfo"], function (result) {
-    if (!result.userInfo) {
-      fetchUserInfo(apiKey);
-    }
-  });
-});
+  // Save username to local storage
+  localStorage.setItem("showwcase-username", username);
+  console.log("Username saved to local storage");
 
-// Retrieve API key from Chrome storage
-chrome.storage.local.get("showwcase-api-key", function (data) {
-  if (data["showwcase-api-key"]) {
-    apiKeyInputOptionsPage.value = data["showwcase-api-key"];
+  const userInfo = localStorage.getItem("userInfo");
+  if (!userInfo) {
+    fetchUserInfo(apiKey);
   }
 });
 
-// Retrieve username from Chrome storage
-chrome.storage.local.get("showwcase-username", function (data) {
-  if (data["showwcase-username"]) {
-    usernameInputOptionsPage.value = data["showwcase-username"];
-  }
-});
+// Retrieve API key from local storage
+const apiKey = localStorage.getItem("showwcase-api-key");
+if (apiKey) {
+  apiKeyInputOptionsPage.value = apiKey;
+}
+
+// Retrieve username from local storage
+const username = localStorage.getItem("showwcase-username");
+if (username) {
+  usernameInputOptionsPage.value = username;
+}
 //! Options Page JS END
--- js/script.js
+++ js/script.js
@@ -1,80 +1,57 @@
-// Check in the browser local storage if the user has already saved an API key if true then #user-loggedin block will be displayed
-chrome.storage.local.get(["showwcase-api-key"], function (data) {
-  if (data["showwcase-api-key"]) {
-    document.getElementById("user-loggedin").style.display = "block";
-    document.getElementById("user-not-loggedin").style.display = "none";
-  } else {
-    document.getElementById("user-loggedin").style.display = "none";
-    document.getElementById("user-not-loggedin").style.display = "block";
-    // Form submit event listener
-    form.addEventListener("submit", (event) => {
-      event.preventDefault(); // Prevent form submission
-
-      const apiKeyInput = document.getElementById("api-key-input");
-      const usernameInput = document.getElementById("api-username-input");
-
-      const apiKey = apiKeyInput.value;
-      const username = usernameInput.value;
-
-      console.log("API Key:", apiKey);
-      console.log("Username:", username);
-
-      // Save API key to Chrome storage
-      chrome.storage.local.set({ "showwcase-api-key": apiKey }, function () {
-        // Change the text to submitted but not change the svg icon in the btn
-        formSubmitBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
-		<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
-	  </svg> Submitted!`;
-        // Change the text back to submit after 3 seconds
-        setTimeout(() => {
-          formSubmitBtn.innerHTML = `<svg class="paper-plane" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-			aria-hidden="true">
-			<path
-				d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z">
-			</path>
-		</svg> Submit`;
-        }, 2000);
-        console.log("API key saved to Chrome storage");
-      });
-
-      // Save username to Chrome storage
-      chrome.storage.local.set({ "showwcase-username": username }, function () {
-        console.log("Username saved to Chrome storage");
-      });
-      chrome.storage.local.get(["userInfo"], function (result) {
-        if (!result.userInfo) {
-          fetchUserInfo(apiKey);
-        }
-      });
-    });
-
-    // Retrieve API key from Chrome storage
-    chrome.storage.local.get("showwcase-api-key", function (data) {
-      if (data["showwcase-api-key"]) {
-        apiKeyInput.value = data["showwcase-api-key"];
-      }
-    });
-
-    // Retrieve username from Chrome storage
-    chrome.storage.local.get("showwcase-username", function (data) {
-      if (data["showwcase-username"]) {
-        usernameInput.value = data["showwcase-username"];
-      }
-    });
-  }
-});
-
 const postBtn = document.getElementById("post-submit-btn");
 const apiKeyInput = document.getElementById("api-key-input");
-const usernameInput = document.getElementById("api-username-input");
+
 const apiKeyModal = document.getElementById("api-key-modal");
 const createPostBtn = document.getElementById("create-post-btn");
 const postsListBtn = document.getElementById("posts-list-btn");
 const settingBtn = document.getElementById("setting-btn");
 const form = document.getElementById("api-username-form");
 const formSubmitBtn = document.getElementById("save-api-key-btn");
+let apiKeyAfterSubmit = "";
 let userInformationFromNotification = "";
 let isPostLoaded = false;
+// Check in the browser local storage if the user has already saved an API key if true then #user-loggedin block will be displayed
+const savedApiKey = localStorage.getItem("showwcase-api-key");
+const savedUsername = localStorage.getItem("showwcase-username");
+if (savedApiKey) {
+  document.getElementById("user-loggedin").style.display = "block";
+  document.getElementById("user-not-loggedin").style.display = "none";
+  // Logout user by removing the API key from local storage
+  const logoutBtn = document.getElementById("log-out");
+  logoutBtn.addEventListener("click", () => {
+    localStorage.removeItem("showwcase-api-key");
+    localStorage.removeItem("showwcase-username");
+    localStorage.removeItem("userInfo");
+    console.log("API key removed from local storage");
+    console.log("Username removed from local storage");
+    window.location.href = "popup.html";
+  });
+} else {
+  document.getElementById("user-loggedin").style.display = "none";
+  document.getElementById("user-not-loggedin").style.display = "block";
+  // Form submit event listener
+  form.addEventListener("submit", (event) => {
+    event.preventDefault(); // Prevent form submission
+    const apiKeyInput = document.getElementById("api-key-input");
+    const apiKey = apiKeyInput.value;
+    console.log("API Key:", apiKey);
+    // Save API key to local storage
+    localStorage.setItem("showwcase-api-key", apiKey);
+    console.log("API key saved to local storage");
+    // After Submit the form, fetch and save the user info to local storage
+    fetchUserInfo(apiKey);
+
+    document.getElementById("user-loggedin").style.display = "block";
+    document.getElementById("user-not-loggedin").style.display = "none";
+  });
+
+  // Retrieve API key from local storage
+  const savedApiKey = localStorage.getItem("showwcase-api-key");
+  if (savedApiKey) {
+    apiKeyInput.value = savedApiKey;
+  }
+}
+
 function getCurrentUrl() {
   return new Promise((resolve, reject) => {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -86,20 +63,19 @@ function getCurrentUrl() {
     });
   });
 }
-// Function to get saved user info to chrome storage
-function getSavedUserInfoFromChromeStorage() {
-  chrome.storage.local.get(["userInfo"], function (result) {
-    if (result.userInfo) {
-      const userJson = result.userInfo;
-      const user = JSON.parse(userJson);
-      userInformationFromNotification = user;
-      console.log(userInformationFromNotification);
-    }
-  });
+// Function to get saved user info from local storage
+function getSavedUserInfoFromLocalStorage() {
+  const userJson = localStorage.getItem("userInfo");
+  if (userJson) {
+    const user = JSON.parse(userJson);
+    userInformationFromNotification = user;
+    console.log("afterSave", userInformationFromNotification);
+  }
 }
+
 // Fetch user info function from Notification
 function fetchUserInfo(apiKey) {
-  fetch("https://cache.showwcase.com/notifications", {
+  fetch("https://cache.showwcase.com/auth", {
     headers: {
       "Content-Type": "application/json",
       "x-api-key": apiKey,
@@ -112,11 +88,12 @@ function fetchUserInfo(apiKey) {
       return response.json();
     })
     .then((data) => {
-      if (data[0].data.thread) {
-        const user = data[0].data.thread.user;
+      if (data) {
+        const user = data;
+        console.log("from response", data);
         const userJson = JSON.stringify(user);
-        chrome.storage.local.set({ userInfo: userJson });
-        getSavedUserInfoFromChromeStorage();
+        localStorage.setItem("userInfo", userJson);
+        getSavedUserInfoFromLocalStorage();
       }
     })
     .catch((error) => {
@@ -150,7 +127,7 @@ function addActiveClass(btnOrder) {
 function postFetchFunc(title, description, url, apiKey, callback) {
   const body = {
     title: title ? title : "",
-    message: description,
+    message: description? description : alert("Please enter a description"),
     linkPreviewUrl: url ? url : "",
   };
   fetch("https://cache.showwcase.com/threads", {
@@ -183,7 +160,7 @@ function postFetchFunc(title, description, url, apiKey, callback) {
 postBtn.addEventListener("click", () => {
   const postTitle = document.getElementById("post-title").value;
   const postDescription = document.getElementById("post-description").value;
-  const apiKey = apiKeyInput.value;
+
   const urlCheckbox = document.getElementById("url-checkbox");
   if (!apiKey) {
     alert("Please provide an API key");
@@ -203,7 +180,7 @@ postBtn.addEventListener("click", () => {
 });
 // Add boost event listener to every boost post btn
 function addBoostEventListener(postId, isBoostedByUser, boostedByArrayLength) {
-  const apiKey = apiKeyInput.value;
+  const apiKey = localStorage.getItem("showwcase-api-key"); // Retrieve API key from local storage
   let afterBoostedbyUser = isBoostedByUser;
   if (!apiKey) {
     alert("Please provide an API key");
@@ -252,88 +229,91 @@ function addBoostEventListener(postId, isBoostedByUser, boostedByArrayLength) {
 // Fetch posts list and display them in the id="posts-list" div
 function fetchPostsList() {
   const postsList = document.getElementById("posts-list");
-  getSavedUserInfoFromChromeStorage();
-  // Get username from Chrome storage
-  chrome.storage.local.get("showwcase-username", function (result) {
-    const storedUsername = result["showwcase-username"];
-    if (!storedUsername) {
-      console.error("Username not found in Chrome storage");
-      return;
-    }
+  getSavedUserInfoFromLocalStorage();
 
-    const username = storedUsername;
+  // Get username from local storage
+  const storedUsername = localStorage.getItem("showwcase-username");
+  if (!storedUsername) {
+    console.error("Username not found in local storage");
+    return;
+  }
 
-    fetch(
-      `https://cache.showwcase.com/threads/?username=${
-        userInformationFromNotification.username || username
-      }&limit=15`
-    )
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        const posts = data;
-        if (!isPostLoaded) {
-          posts.forEach((post) => {
-            console.log(post);
-            const boostedByArray = post.boostedBy;
-            const boostedByArrayLength = boostedByArray.length;
-            let isBoostedByUser = false;
-            for (let i = 0; i < boostedByArray.length; i++) {
-              const user = boostedByArray[i];
-              if (
-                user.username === userInformationFromNotification.username ||
-                user.username === username
-              ) {
-                isBoostedByUser = true;
-                break;
-              }
+  const username = storedUsername;
+
+  fetch(
+    `https://cache.showwcase.com/threads/?username=${
+      userInformationFromNotification.username || username
+    }&limit=15`
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const posts = data;
+      if (!isPostLoaded) {
+        posts.forEach((post) => {
+          console.log(post);
+          const boostedByArray = post.boostedBy;
+          const boostedByArrayLength = boostedByArray.length;
+          let isBoostedByUser = false;
+          for (let i = 0; i < boostedByArray.length; i++) {
+            const user = boostedByArray[i];
+            if (
+              user.username === userInformationFromNotification.username ||
+              user.username === username
+            ) {
+              isBoostedByUser = true;
+              break;
             }
-            // Insert every post message into the postsList div
-            const postMessage = `
-						<div class="single-post">
-							<div class="profile-img"><img src="${post.user.profilePictureKey}"></div>
-							<div class="post-content">
-								<a href="https://www.showwcase.com/thread/${post.id}" target="_blank">
-									<h2 class="post-title">${post.title ? post.title : ""}</h2>
-									<p class="post-message">${post.message}</p>
-								</a>
-							</div>
-							<div class="boost-post-btn">
-								<svg class="boost-post-svg" post-id="${post.id}" id="svg-${
-              post.id
-            }" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="${
-              isBoostedByUser ? "#27ae60" : "currentColor"
-            }" aria-hidden="true">
-									<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z" clip-rule="evenodd"></path>
-								</svg>
-								<span id="boost-count-${
+          }
+          // Insert every post message into the postsList div
+          const postMessage = `
+            <div class="single-post">
+              <div class="profile-img"><img src="${
+                post.user.profilePictureKey
+              }"></div>
+              <div class="post-content">
+                <a href="https://www.showwcase.com/thread/${
+                  post.id
+                }" target="_blank">
+                  <h2 class="post-title">${post.title ? post.title : ""}</h2>
+                  <p class="post-message">${post.message}</p>
+                </a>
+              </div>
+              <div class="boost-post-btn">
+                <svg class="boost-post-svg" post-id="${post.id}" id="svg-${
+            post.id
+          }" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="${
+            isBoostedByUser ? "#27ae60" : "currentColor"
+          }" aria-hidden="true">
+                  <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z" clip-rule="evenodd"></path>
+                </svg>
+                <span id="boost-count-${
                   post.id
                 }" class="boost-count">${boostedByArrayLength}</span>
-							</div>
-						</div>`;
+              </div>
+            </div>`;
 
-            postsList.insertAdjacentHTML("beforeend", postMessage);
-            document
-              .getElementById("svg-" + post.id)
-              .addEventListener("click", () => {
-                addBoostEventListener(
-                  post.id,
-                  isBoostedByUser,
-                  boostedByArrayLength
-                );
-              });
-          });
-          isPostLoaded = true;
-        }
-      })
-      .catch((error) => {
-        console.error("There was a problem with the fetch operation:", error);
-      });
-  });
+          postsList.insertAdjacentHTML("beforeend", postMessage);
+          document
+            .getElementById("svg-" + post.id)
+            .addEventListener("click", () => {
+              addBoostEventListener(
+                post.id,
+                isBoostedByUser,
+                boostedByArrayLength
+              );
+            });
+        });
+        isPostLoaded = true;
+      }
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
 }
 
 createPostBtn.addEventListener("click", () => {
