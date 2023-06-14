9e993dd41bc8c00f4c81326f87ab36dff0c700f1
nomandhoni-cs <nomandhoni@yahoo.com>
2023-06-14 20:23:03 +0600

    Profile Section done

--- js/script.js
+++ js/script.js
@@ -459,6 +459,12 @@ async function loadProfileInfo() {
 <svg width="30" height="25" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"><path fill-rule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z" clip-rule="evenodd"></path></svg>
   <a href="${resume}" target="_blank">Resume</a>
 </div>
+  <div class="footer-logo-and-name">
+  <a href="https://github.com/nomandhoni-cs/Showwand" target="_blank">
+      <img src="assets/icon48.png" alt="Showwand Logo" class="logo">
+      <span class="logo-name">Showwand</span>
+  </a>
+  </div>
 `;
 if (!isProfileLoaded){
   profileContainer.insertAdjacentHTML("beforeend", profileSceleton);
